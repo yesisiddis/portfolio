a6f2e08d3cfd41084b334f5434f104ca80ec9665
yesisiddis <52675633+yesisiddis@users.noreply.github.com>
2020-01-25 22:05:53 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -90,4 +90,49 @@ $(function() {
   });
 });
 
-//////////////////////////////////////////////////
+////////////////////////   Smooth Scroll    //////////////////////////
+
+document.addEventListener("click", function(e) {
+  if (e.target.tagName !== "A") return;
+
+  if (
+    e.target.href &&
+    e.target.href.indexOf("#") != -1 &&
+    (e.target.pathname == location.pathname ||
+      "/" + e.target.pathname == location.pathname) &&
+    e.target.search == location.search
+  ) {
+    scrollAnchors(e, e.target);
+  }
+});
+
+function scrollAnchors(e, respond = null) {
+  function distanceToTop(el) {
+    return Math.floor(el.getBoundingClientRect().top);
+  }
+
+  e.preventDefault();
+  var targetID = respond
+    ? respond.getAttribute("href")
+    : this.getAttribute("href");
+  var targetAnchor = document.querySelector(targetID);
+  if (!targetAnchor) return;
+  var originalTop = distanceToTop(targetAnchor);
+  window.scrollBy({ top: originalTop, left: 0, behavior: "smooth" });
+  var checkIfDone = setInterval(function() {
+    var atBottom =
+      window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+    if (distanceToTop(targetAnchor) === 0 || atBottom) {
+      targetAnchor.tabIndex = "-1";
+      targetAnchor.focus();
+
+      if ("history" in window) {
+        window.history.pushState("", "", targetID);
+      } else {
+        window.location = targetID;
+      }
+
+      clearInterval(checkIfDone);
+    }
+  }, 100);
+}
